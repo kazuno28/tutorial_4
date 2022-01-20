4dc607968b23f2e0f85e7dc6971e77566de41101
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-01-20 07:33:17 +0000

    about追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+
+  test "should get home" do  #Homeページのテスト。
+    get static_pages_home_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
+    assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get help" do  #Helpページのテスト。
+    get static_pages_help_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
+    assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
+  end
+  
+  test "should et about" do
+    get static_pages_about_url
     assert_response :success
   end
-
 end
