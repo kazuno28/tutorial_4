ecfa27af93eef9fe453bc45308ec09a7221138c5
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-01-24 07:30:54 +0000

    root変更・testコード編集

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,18 +5,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get home" do  #Homeページのテスト。
-    get static_pages_home_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
-    assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
   end
 
-  test "should get help" do  #Helpページのテスト。
-    get static_pages_help_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
-    assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  end
+  #test "should get help" do  #Helpページのテスト。
+    #get static_pages_help_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
+    #assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
+    #assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  #end
 
   test "should et about" do
     get static_pages_about_url
