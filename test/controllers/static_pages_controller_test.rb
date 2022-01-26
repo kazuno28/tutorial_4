5f9e41fecd22552e9af1dd7d839f2f5ae4b7d6e8
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-01-26 08:05:13 +0000

    titleカスタムヘルパー

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  
-  test "should get root" do
-    get root_url
+
+  #test "should get root" do
+    #get root_url
+    #assert_response :success
+  #end
+
+  test "should get home" do
+    get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
-  #test "should get help" do  #Helpページのテスト。
-    #get static_pages_help_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
-    #assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
-    #assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  #end
+  test "should get help" do  #Helpページのテスト。
+    get static_pages_help_url  #GETリクエストをhomeアクションに対して発行 (=送信) せよ。
+    assert_response :success  #そうすれば、リクエストに対するレスポンスは[成功]になるはず
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
 
   test "should et about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
