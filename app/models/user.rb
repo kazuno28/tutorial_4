1bb4998f8eef801a22af58773cfca3819bd56e5a
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-03-01 07:24:56 +0000

    セキュアなパスワードを追加する・演習

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   #before_save { self.email = email.downcase }
   before_save { email.downcase! }
-  validates :name,  presence: true, length: {maximum: 50}
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  #VALID_EMAIL_REGEXは定数。大文字で始まる名前はRubyでは定数を意味する。
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
