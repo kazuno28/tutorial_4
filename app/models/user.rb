26e65d14f7556eaf564d9f8ad8fc90b9ae17cfa3
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-03-19 05:56:09 +0000

    記憶トークンと暗号化・演習

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,36 @@
 class User < ApplicationRecord
   #before_save { self.email = email.downcase }
-  before_save { email.downcase! }
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i  #VALID_EMAIL_REGEXは定数。大文字で始まる名前はRubyでは定数を意味する。
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+ #class << self #[リスト9.5]
   # 渡された文字列のハッシュ値を返す
+  #def self.digest(string) #[リスト9.4]
+  #def digest(string) #[リスト9.5]
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  #def self.new_token #[リスト9.4]
+  #def new_token #[リスト9.5]
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+ #end  #[リスト9.5]
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
