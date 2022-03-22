33f51ff29ee038ae7ffdf6e0de694cec6f7173c9
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-03-22 09:17:02 +0000

    ユーザーを忘れる

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,7 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-
+  
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
