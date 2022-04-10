63adce4cbc2767949036c00ec26c587b1ed312eb
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2022-04-10 06:09:05 +0000

    ユーザーの一覧ページ・演習

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -23,6 +28,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
-  
+
+
 end
